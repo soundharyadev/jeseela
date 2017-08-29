f89af41fdfa545248b33e81077666acd3227ca8d
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2017-08-29 15:12:21 +0530

    Create onebyonestar.c

---
+++ onebyonestar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int n, a, b;
+ 
+    printf("Enter number of rows\n");
+    scanf("%d",&n);
+ 
+    for ( a = 1 ; a <= n ; a++ )
+    {
+        for( b = 1 ; b <= c ; b++ )
+            printf("*");
+ 
+        printf("\n");
+    }
+ 
+    return 0;
+    }
